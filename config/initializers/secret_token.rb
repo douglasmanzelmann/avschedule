608296e9a5d0c107a4ca9e50a621020e7c33f147
douglas <dougamanzelmann@gmail.com>
2012-09-04 16:51:48 -0400

    rails new
    generated the app structure. changed the gemfile.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Avschedule::Application.config.secret_token = '9851f815903711821b494677db29903cd17f79b94be7236ee897074c18d87d7736b807ceb8b82d44cdf8a1bd8d0b516b901195db408b16bd55748f59bd061b20'
