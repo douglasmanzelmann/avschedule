aafab3e461e02d62501cc03242381b49c50618a9
douglas <dougamanzelmann@gmail.com>
2012-09-25 12:18:19 -0400

    re-did check_room_schedule
    probably needs to be renamed...

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ActiveRecord::Base
+  include ApplicationHelper
   attr_accessible :begin, :day, :end, :gear, :notes, :preset, :room, :setup, :strike
   validates :room, presence: true
   validates :begin, presence: true
@@ -14,19 +15,38 @@ class Task < ActiveRecord::Base
   scope :gear_stats, lambda { |gear| where(gear: gear) } 
 
   def check_room_schedule
-    @conflicting_tasks = Task.for_date(self.day).for_room(self.room).list_in_asc_order
 
-    @conflicting_tasks.each_with_index do |task, index|
-      if self.begin < task.begin
-        self.notes = "There is another meeting in this room at #{task.begin.strftime("%I:%M%P")}."
-        break
-      elsif (self.begin > task.begin)
-        if ((index == @conflicting_tasks.length-1) || task.begin > @conflicting_tasks[index + 1].begin)
-          task.update_attributes(notes: "There is another meeting in this room at #{self.begin.strftime("%I:%M%P")}")
-        end
+    #### queries the DB for tasks on the same day and in the same room as the soon-to-be added task. ####
+    #### will soon add equipment, too. ####
+    #### loads the tasks in an array, @room_tasks. ####
+    #### checks to see if self is the first task in the room on the day, updates the notes attrib and breaks. ####
+    #### if it's not the first, checks the current task's index to see if it's the last task in the DB ####
+    #### or if the current task's begin time is later than the next to avoid all tasks adding self.begin to their notes. ####
+
+    @room_tasks = Task.for_date(self.day).for_room(self.room).list_in_asc_order
+    @room_tasks << self
+    @room_tasks = sort_by_begin_time(@room_tasks)
+    @self_position = @room_tasks.index(self)
+    self.preset = "self position: #{@self_position}"
+
+    if @room_tasks.length >= 1
+      if @self_position == 0 #### Self is the first element in the array ####
+        self.notes = "There is another meeting in this room at #{@room_tasks[@self_position+1].begin.strftime("%I:%M%P")}."
+
+        @last_room_task_yesterday = Task.for_date(self.day.yesterday).for_room(self.room).list_in_asc_order.last
+        if @last_room_task_yesterday
+          @last_room_task_yesterday.update_attributes(notes: 
+            "There is another meeting in this room tomorrow at #{self.begin.strftime("%I:%M%P")}.")
+        end    
+      elsif @self_position == @room_tasks.length-1 #### Self is the last element in the array ####
+        @room_tasks[@self_position-1].update_attributes(
+            notes: "There is another meeting in this room at #{self.begin.strftime("%I:%M%P")}.")
+      else
+        @room_tasks[@self_position-1].update_attributes(
+          notes: "There is another meeting in this room at #{self.begin.strftime("%I:%M%P")}.")
+        self.notes = "There is another meeting in this room at #{@room_tasks[@self_position+1].begin.strftime("%I:%M%P")}."
       end
     end
-
   end
 
   private
